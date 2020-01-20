9418806f46cdacf55393beed59af939cbcb4eb5d
George Kour <gkour@ibm.com>
2020-01-20 09:57:22 +0200

    raters to raster typo fixing.

--- Neural Decoding/plotClassifierResults.m
+++ Neural Decoding/plotClassifierResults.m
@@ -1,12 +1,12 @@
-function plotClassifierResults(ratersDir, event, toDecode, EnoughCellsINX, numSplits)
+function plotClassifierResults(rastersDir, event, toDecode, EnoughCellsINX, numSplits)
 %PLOTCLASSIFIERRESULTS Summary of this function goes here
 %   plotClassifierResults('C:\Users\GEORGEKOUR\Desktop\Electro_Rats\Rasters','Ain', 'Rewarded',EnoughCellsINX ,20)
 
 
-decoding_results = fullfile(ratersDir,[event,'_',toDecode,'_Results']);
+decoding_results = fullfile(rastersDir,[event,'_',toDecode,'_Results']);
 
 figure;
-result_names{1} = fullfile(ratersDir,[event,'_',toDecode,'_Results']);
+result_names{1} = fullfile(rastersDir,[event,'_',toDecode,'_Results']);
 plot_obj = plot_standard_results_object(result_names, '');
 
 plot_obj.significant_event_times = 1000;
@@ -17,7 +17,7 @@ plot_obj.errorbar_stdev_multiplication_factor = 1/sqrt(numSplits); %to make it S
 
 plot_obj.null_distribution_file_prefix_name=[event,'_',toDecode];
 
-pval_dir{1} = fullfile(ratersDir,'Shuffle');
+pval_dir{1} = fullfile(rastersDir,'Shuffle');
 plot_obj.p_values = pval_dir;
 plot_obj.collapse_all_times_when_estimating_pvals = 1;
 plot_obj.p_value_alpha_level = 0.05;
