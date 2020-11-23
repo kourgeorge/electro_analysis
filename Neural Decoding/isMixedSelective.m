6513f19d3797a3b40d8974056dcc433cf2daa73c
George Kour <gkour@ibm.com>
2020-11-23 10:08:09 +0200

    fix the mix selectivity and add baseline reduction option.

--- Neural Decoding/isMixedSelective.m
+++ Neural Decoding/isMixedSelective.m
@@ -1,30 +1,34 @@
-function is_selective = isMixedSelective(  cell_data, labels , baseline_range_bins, target_range_bins, alpha, min_consecutive_bins )
+function [is_selective,pvals,pvals2] = isMixedSelective(cell_data, labels , baseline_range_bins, target_range_bins, alpha, min_consecutive_bins )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-[combinations , ~, targets, ~ ] = getCellBaselineTargetFR( cell_data, labels , baseline_range_bins, target_range_bins );
+[combinations , baseline, targets, ~ ] = getCellBaselineTargetFR( cell_data, labels , baseline_range_bins, target_range_bins );
 
 pvals = [];
 pval2 = [];
 num_bins_in_target = target_range_bins(2)-target_range_bins(1)+1;
+alpha_selectivity = (alpha/(target_range_bins(2)-target_range_bins(1)-min_consecutive_bins+2))^(1/min_consecutive_bins);
+test = [];
 for i=1:num_bins_in_target
     % for each label
     samples = [];
     groups = [];
+    
     for v=1:length(targets)
-        bin_samples = targets{v}(:,i);
+        bin_samples = targets{v}(:,i);%-baseline{v};
         samples = [samples;bin_samples];
         groups = [groups; repmat(combinations(:,v)', length(bin_samples), 1)];
+        test(v,i) = mean(bin_samples);
     end
     % perform anova between samples values in the same bin
-    [pval,table,stats]= anovan(samples,groups,'alpha',0.05,'model','interaction','display', 'off');
-    [pval2,table,stats]= anovan(samples,groups,'alpha',0.05,'model','interaction', 'sstype', 2,'display', 'off');
+    [pval,table,stats]= anovan(samples,groups,'model','interaction','display', 'off');
+    [pval2,table,stats]= anovan(samples,groups, 'sstype', 2,'display', 'off');
     pvals(:,i) = pval;
     pvals2(:,i) = pval2;
 end
 
-significance = pvals<alpha;
-significance2 = pvals2<alpha;
+significance = pvals<alpha_selectivity;
+significance2 = pvals2<alpha_selectivity;
 significance(isnan(significance))=0;
 significance2(isnan(significance2))=0;
 
@@ -33,8 +37,8 @@ if (longestConsecutiveOnes(significance(3,:))>=min_consecutive_bins)
 elseif (longestConsecutiveOnes(significance2(1,:))>=min_consecutive_bins) && ...
         (longestConsecutiveOnes(significance2(2,:))>=min_consecutive_bins)
     is_selective = 'LMS';
-elseif (longestConsecutiveOnes(significance(1,:))>=min_consecutive_bins) || ...
-    (longestConsecutiveOnes(significance(2,:))>=min_consecutive_bins) 
+elseif (longestConsecutiveOnes(significance2(1,:))>=min_consecutive_bins) || ...
+    (longestConsecutiveOnes(significance2(2,:))>=min_consecutive_bins) 
     is_selective = 'SS';
 else
     is_selective = 'No';
