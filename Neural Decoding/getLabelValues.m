e90f9836a5b0cbb696d299f5d5244c5ddbb8ebca
George Kour <kourgeorge@gmail.com>
2021-05-04 10:17:32 +0300

    Move get label value

---
+++ Neural Decoding/getLabelValues.m
@@ -0,0 +1,25 @@
+function allPossibilities = getLabelValues(label)
+
+switch label
+    case 'Chosen'
+        allPossibilities = [1 2 3 4];
+    case 'NPChosen'
+        allPossibilities = [1 2 3 4];
+    case 'CorrectArm1'
+        allPossibilities = [1 2];
+    case 'CorrectArm2'
+        allPossibilities = [3 4];
+    case 'CorrectArm'
+        allPossibilities = [1 1];
+    case 'Rewarded'
+        allPossibilities = [0 1];
+    case 'NPRewarded'
+        allPossibilities = [0 1];
+    case 'ArmType'
+        allPossibilities = [1 2];
+    case 'combination'
+        allPossibilities = [{'right_R_water','left_R_water', 'right_NR_water','left_NR_water'...
+            'right_R_food','left_R_food', 'right_NR_food','left_NR_food'}];
+end
+
+end
