f8180484f6b537953c467982204c06258ed5853a
George Kour <kourgeorge@gmail.com>
2021-08-29 13:22:17 +0300

    add confusions of the non reduced data.

--- Neural Decoding/plot_population.m
+++ Neural Decoding/plot_population.m
@@ -23,8 +23,6 @@ for bin=bins
     
     for rep=1%:length(all_XTr{bin})
         
-        %XTr = all_XTr{bin}{rep}';
-        %XTe = all_XTe{bin}{rep}';
         
         XTr = all_XTr{bin}{1}';
         XTe = all_XTe{bin}{1}';
@@ -32,93 +30,87 @@ for bin=bins
         YTe = all_YTe; 
         
         [num_vec, population_size] = size(XTr); 
-
-%        dim=get_dimensionality(XTr)
-%        dim2=get_dimensionality(XTe)
-
-%        extract_geometry(XTr);
         
         
         YX = [[YTr;YTe],[XTr;XTe]];
-        %YX( :, ~any(YX,1) ) = [];
        
         X = [XTr;XTe];
         method = 'PCA';
-        [x_dm, mapping] = compute_mapping(X, method, 2);
+        [XTr_XTe_reduced, mapping] = compute_mapping(X, method, 2);
         
         
-        x_t = x_dm(1:end-length(YTe),:);
-        x_e = x_dm(end-length(YTe)+1:end,:);
+        XTr_reduced = XTr_XTe_reduced(1:end-length(YTe),:);
+        XTe_reduced = XTr_XTe_reduced(end-length(YTe)+1:end,:);
         
-        % Learn DR on train and apply on test
-%         [COEFF, SCORE] = pca(XTr);
-%         x_t = SCORE(:,1:2);
-%         x_e = XTe*COEFF;
-%         x_e= x_e(:,1:2);
-%         x_dm = [x_t;x_e];
         
+        %%%Plot the population in 2d%%%%%
         figure;
-        gscatter(x_t(:,1),x_t(:,2),YTr, 'br','oo')
+        gscatter(XTr_reduced(:,1),XTr_reduced(:,2),YTr, 'br','oo')
         hold on 
-        gscatter(x_e(:,1),x_e(:,2),YTe, 'br','xx')
+        gscatter(XTe_reduced(:,1),XTe_reduced(:,2),YTe, 'br','xx')
         
         %%%%%%%MC%%%%%%%
         MC = max_correlation_coefficient_CL;
-        MC = MC.train(x_t', YTr);
-
-     
+        MC = MC.train(XTr_reduced', YTr);
         scatter(MC.templates(1,1), MC.templates(2,1),'r', 'filled');
         scatter(MC.templates(1,2), MC.templates(2,2),'b', 'filled'); 
         
-               %%%%SVM%%%% 
+        %%%%%%%SVM%%%%%%% 
+        %%% Plot training hyperplane%%%
         SVM =libsvm_CL;
         SVM.kernel = 'linear';
-        SVM = SVM.train(x_t', YTr);
-        h = plot_svm_boundry(SVM, 0.8*min(x_dm(:,1)),0.8*max(x_dm(:,1)), 'g--');
-        ylim([min(x_dm(:,2)), max(x_dm(:,2))])
+        SVM = SVM.train(XTr_reduced', YTr);
+        h = plot_svm_boundry(SVM, 0.8*min(XTr_XTe_reduced(:,1)),0.8*max(XTr_XTe_reduced(:,1)), 'g--');
+        ylim([min(XTr_XTe_reduced(:,2)), max(XTr_XTe_reduced(:,2))])
         h.DisplayName = 'Train SVM';
         
-
-        
-        
+        %%% Plot training hyperplane%%%
         SVM2 =libsvm_CL;
         SVM2.kernel = 'linear';
-        SVM2 = SVM2.train(x_e', YTe);
-        h = plot_svm_boundry(SVM2, 0.8*min(x_dm(:,1)),0.8*max(x_dm(:,1)), 'c--');
-        ylim([min(x_dm(:,2)), max(x_dm(:,2))])
+        SVM2 = SVM2.train(XTe_reduced', YTe);
+        h = plot_svm_boundry(SVM2, 0.8*min(XTr_XTe_reduced(:,1)),0.8*max(XTr_XTe_reduced(:,1)), 'c--');
+        ylim([min(XTr_XTe_reduced(:,2)), max(XTr_XTe_reduced(:,2))])
         h.DisplayName = 'Test SVM';
 
-       
-        
-%         mdl = fitcsvm(x_t,YTr,'KernelFunction', 'linear');
-%         y_hat_svm_m = predict(mdl,x_e);
-%         x_p = linspace(min(x_t(:,1)),max(x_t(:,1)));
-%         f = @(x_p) -(x_p*mdl.Beta(1) + mdl.Bias)/mdl.Beta(2);
-%         y_p = f(x_p);
-%         plot(x_p,y_p,'r--','LineWidth',2,'DisplayName','Boundary')
-
-        [y_hat_mc, ~] = MC.test(x_e');
-        [y_hat_svm, ~] = SVM.test(x_e');
+        %Calculate generatlization accuracy on reduced data %%%%%
+        [y_hat_mc, ~] = MC.test(XTe_reduced');
+        [y_hat_svm, ~] = SVM.test(XTe_reduced');
         acc_MC = mean(y_hat_mc==YTe);
-        acc_SVM = mean(y_hat_svm==YTe);
-
-        
+        acc_SVM = mean(y_hat_svm==YTe);        
         title(['Population size: ', num2str(population_size), '; N=', num2str(num_vec), ...
             ' ; bin', num2str(bin),' ; MC/SVM:', num2str(acc_MC), '/', num2str(acc_SVM), ' ', ])
         
                 
         legend('Train, label1','Train, label2','Test, label1','Test, label2','M label1', 'M labels2', 'Train boundary', 'Test boundary')
         hold off
+       
         
-        figure;     
-        y_pred_tr_te=SVM.test(x_e');
+        
+        %%% Plot confusion matrix %%%%
+        figure;
+        subplot(2,2,1)
+        y_pred_tr_te=SVM.test(XTe_reduced');
         C = confusionmat(YTe,y_pred_tr_te);
+        heatmap(C)
+        title('confusion of test data (reduced) on train SVM')
+        
+        subplot(2,2,2)
+        y_pred_te_tr=SVM2.test(XTr_reduced');
+        C = confusionmat(YTr,y_pred_te_tr);
+        heatmap(C)
+        title('confusion of train data (reduced) on test SVM')
+        
+        subplot(2,2,3)
+        SVM = train_svm(XTr',YTr);
+        y_pred=SVM.test(XTe');
+        C = confusionmat(YTe,y_pred);
         heatmap(C)
         title('confusion of test data on train SVM')
         
-        figure;
-        y_pred_te_tr=SVM2.test(x_t');
-        C = confusionmat(YTr,y_pred_te_tr);
+        subplot(2,2,4)
+        SVM = train_svm(XTe',YTe);
+        y_pred=SVM.test(XTr');
+        C = confusionmat(YTe,y_pred);
         heatmap(C)
         title('confusion of train data on test SVM')
 
@@ -127,9 +119,16 @@ end
 
 end
 
+
+function model = train_svm(X,y)
+
+SVM =libsvm_CL;
+SVM.kernel = 'linear';
+model = SVM.train(X, y);
+
+end
+
 function h = plot_svm_boundry(SVM, lim1,lim2,line)
-
-
 w = SVM.model.SVs' * SVM.model.sv_coef;
 b = -SVM.model.rho;
 
@@ -151,11 +150,4 @@ function dim = get_dimensionality(x)
 
 dim = find(cumsum(explained)>95,1);
 
-end
-
-
-function geometry(X,y)
-    mat = cov(X);
-    [V,D] = eig(mat)
-    
 end
