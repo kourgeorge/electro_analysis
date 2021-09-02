648811d67253ab9ab912da5186176ff9bac54e82
George Kour <kourgeorge@gmail.com>
2021-09-02 11:29:29 +0300

    improve thw dimensionality reduction of ds.

--- Neural Decoding/dimensionality_reduce_ds.m
+++ Neural Decoding/dimensionality_reduce_ds.m
@@ -4,7 +4,7 @@ function [all_XTr_reduced, all_XTe_reduced] = dimensionality_reduce_ds(ds)
 timebins = length(all_XTr);
 splits = length(all_XTr{1});
 
-%collect all data points of all timebins from both train and test
+%collect all data points of all timebins and folds from both train and test
 allX = [];
 for timebin=1:timebins
     for split = 1:splits
@@ -12,24 +12,26 @@ for timebin=1:timebins
     end
 end
 
+% learn the dimensionality reduction model
 method = 'PCA';
 [~, mapping] = compute_mapping(allX, method, 2);
 
 all_XTr_reduced = [];
 all_XTe_reduced = [];
 
+%create a datasource like train and test samples after dimensionality reduction 
 for timebin=1:timebins
     for split = 1:splits
         XTr_reduced_timebin = [];
-        for j=1:length(all_XTr{timebin}{1})
-            sample = all_XTr{timebin}{1}(:,j)';
+        for fold=1:size(all_XTr{timebin}{1},2)
+            sample = all_XTr{timebin}{1}(:,fold)';
             XTr_reduced_timebin = [XTr_reduced_timebin; out_of_sample(sample, mapping)];
         end
         all_XTr_reduced{timebin,split} = {XTr_reduced_timebin'};
         
         XTe_reduced_timebin = [];
-        for j=1:length(all_XTe{timebin}{1})
-            sample = all_XTe{timebin}{1}(:,j)';
+        for fold=1:size(all_XTe{timebin}{1},2)
+            sample = all_XTe{timebin}{1}(:,fold)';
             XTe_reduced_timebin = [XTe_reduced_timebin; out_of_sample(sample, mapping)];
         end
         all_XTe_reduced{timebin,split} = {XTe_reduced_timebin'};
