fcfb1091d95fc34842d87e331b8f962da5b78177
George Kour <kourgeorge@gmail.com>
2021-11-18 18:02:45 +0200

    update color and support both transfer and non-transfer.

--- Neural Decoding/plot_theoretical_decoding.m
+++ Neural Decoding/plot_theoretical_decoding.m
@@ -2,12 +2,18 @@ function plot_theoretical_decoding(ds, decoding_results)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-aug_factor = 10;
+aug_factor = 0;
 
+transfer = false;
+if ds.from_generalization
+    transfer=true;
+end
+    
+    
 for rep=1:50
-    [all_XTr, all_YTr, all_XTe, all_YTe] = augment_ds(ds, aug_factor); 
-    %[all_XTr, all_YTr, all_XTe, all_YTe] = ds.get_data_MC;
-    m=length(all_YTr)/2;
+    %[all_XTr, all_YTr, all_XTe, all_YTe] = augment_ds(ds, aug_factor); 
+    [all_XTr, all_YTr, all_XTe, all_YTe] = ds.get_data_MC;
+    m=100;%length(all_YTr)/2;
     timebins = length(all_XTr);
     labels = unique(all_YTr);
     
@@ -37,24 +43,43 @@ for rep=1:50
     end
 end
 
+if transfer
+    acc=acc_T;
+end
+%acc=acc_T;
+
 theoretical_acc_mat = mean(acc,2);
+snr_A_rep = acc(:,:,1);
+snr_B_rep = acc(:,:,2);
 theoretical_acc_mat=squeeze(theoretical_acc_mat);
 theoretical_acc = mean(theoretical_acc_mat,2);
-    
 
 figure;
 hold on;
-plot(1:timebins, [theoretical_acc, theoretical_acc_mat], '--', 'LineWidth',2)
-legend('Theory Accuracy', ['Theory recall', num2str(labels(1))], ['Theory recall', num2str(labels(2))],'AutoUpdate','off');
-set(gca,'ColorOrderIndex',1)
-if nargin>1
+h=plot(1:timebins, [theoretical_acc, theoretical_acc_mat], '-', 'LineWidth',2);
+
+h(1).Color = '#EDB120';
+h(2).Color = '#0072BD';
+h(3).Color = '#A2142F';
+
+if transfer
+    legend('Theory Accuracy', ['Theory recall: ', ds.the_test_label_names{labels(1)}{1}], ['Theory recall: ', ds.the_test_label_names{labels(2)}{1}],'AutoUpdate','off', 'Interpreter', 'none');
+    set(gca,'ColorOrderIndex',1)
+end
+
+shadedErrorBar(1:timebins,mean([snr_A_rep,snr_B_rep]'), sem([snr_A_rep,snr_B_rep]'),{'color',h(1).Color},0.5)
+shadedErrorBar(1:timebins,mean(snr_A_rep'), sem(snr_A_rep'),{'color',h(2).Color},0.5)
+shadedErrorBar(1:timebins,mean(snr_B_rep'), sem(snr_B_rep'),{'color',h(3).Color},0.5)
+
+
+if transfer && nargin>1
     confusion_matrix= decoding_results.DECODING_RESULTS.ZERO_ONE_LOSS_RESULTS.confusion_matrix_results.confusion_matrix;
     empirical_acc_mat = [confusion_matrix(1,1,:)./sum((confusion_matrix(:,1,:))),...
         confusion_matrix(2,2,:)./sum((confusion_matrix(:,2,:)))];
 
-    plot(1:timebins, [squeeze(mean(empirical_acc_mat,2)), squeeze(empirical_acc_mat)'], 'LineWidth',2)
-    legend('Theory Accuracy', ['Theory recall', num2str(labels(1))], ['Theory recall', num2str(labels(2))],...
-        'Empirical Accuracy', ['Empirical recall', num2str(labels(1))], ['Empirical recall', num2str(labels(2))],'AutoUpdate','off')
+    plot(1:timebins, [squeeze(mean(empirical_acc_mat,2)), squeeze(empirical_acc_mat)'], '-', 'LineWidth',2)
+    legend('Theory Accuracy', ['Theory recall', ds.the_test_label_names{labels(1)}{1}], ['Theory recall: ', ds.the_test_label_names{labels(2)}{1}],...
+        'Empirical Accuracy', ['Empirical recall', ds.the_test_label_names{labels(1)}{1}], ['Empirical recall: ', ds.the_test_label_names{labels(2)}{1}],'AutoUpdate','off', 'Interpreter', 'none')
 end
 
 plot(1:timebins, repmat(0.5,timebins, 1) , 'k', 'LineWidth',1)
@@ -62,7 +87,7 @@ hold off;
 
 event = ds.get_DS_properties.binned_site_info.event{1};
 subtitle(['Event: ', event, ' Tar.: ' , ds.label, ' Label rep.: ', num2str(ds.num_times_to_repeat_each_label_per_cv_split),' m: ', num2str(m), ...
-    ' #Cells: ', num2str(length(ds.sites_to_use)), ' Augmentation factor: ', num2str(aug_factor), 'Tr/Te: ',num2str(length(all_YTr)),'/',num2str(length(all_YTe))]);
+    ' #Cells: ', num2str(length(ds.sites_to_use)), ' Augmentation factor: ', num2str(aug_factor), ' Tr/Te: ',num2str(length(all_YTr)),'/',num2str(length(all_YTe))]);
 
 
 end
