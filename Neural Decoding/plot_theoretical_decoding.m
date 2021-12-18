8484b150c8133b034881be4ed053e4976ed74cd3
George Kour <kourgeorge@gmail.com>
2021-12-18 14:53:28 +0200

    adding m to theoretical decoding.

--- Neural Decoding/plot_theoretical_decoding.m
+++ Neural Decoding/plot_theoretical_decoding.m
@@ -1,4 +1,4 @@
-function plot_theoretical_decoding(ds, decoding_results)
+function plot_theoretical_decoding(ds, m, decoding_results)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -9,7 +9,6 @@ transfer = isequal(class(ds),'generalization_DS');
 for rep=1:50
     %[all_XTr, all_YTr, all_XTe, all_YTe] = augment_ds(ds, aug_factor); 
     [all_XTr, all_YTr, all_XTe, all_YTe] = ds.get_data_MC;
-    m=100;%length(all_YTr)/2;
     timebins = length(all_XTr);
     labels = unique(all_YTr);
     
@@ -31,8 +30,11 @@ for rep=1:50
         snr_t_A = SNR_T(geom1, geom2, geom3);
         snr_t_B = SNR_T(geom2, geom1, geom4);
         
-        snr_A = SNR(geom1, geom2);
-        snr_B = SNR(geom2, geom1);
+        geom13 = extract_geometry([X1;X3]);
+        geom24 = extract_geometry([X2;X4]);
+        
+        snr_A = SNR(geom13, geom24);
+        snr_B = SNR(geom24, geom13);
 
         acc(timebin,rep,:) = [1-snr_A.error(m), 1-snr_B.error(m)];
         acc_T(timebin, rep,:) = [1-snr_t_A.error(m), 1-snr_t_B.error(m)];
@@ -42,7 +44,6 @@ end
 if transfer
     acc=acc_T;
 end
-acc=acc_T;
 
 theoretical_acc_mat = mean(acc,2);
 snr_A_rep = acc(:,:,1);
@@ -72,7 +73,7 @@ shadedErrorBar(1:timebins,mean(snr_B_rep'), sem(snr_B_rep'),{'color',h(3).Color}
 
 
 % In case of showing the emprirical results.
-if transfer && nargin>1
+if transfer && nargin>2
     confusion_matrix= decoding_results.DECODING_RESULTS.ZERO_ONE_LOSS_RESULTS.confusion_matrix_results.confusion_matrix;
     empirical_acc_mat = [confusion_matrix(1,1,:)./sum((confusion_matrix(:,1,:))),...
         confusion_matrix(2,2,:)./sum((confusion_matrix(:,2,:)))];
@@ -83,12 +84,12 @@ if transfer && nargin>1
 end
 
 plot(1:timebins, repmat(0.5,timebins, 1) , 'k', 'LineWidth',1)
-hold off;
+
 
 event = ds.get_DS_properties.binned_site_info.event{1};
 subtitle(['Event: ', event, ' Tar.: ' , ds.label, ' Label rep.: ', num2str(ds.num_times_to_repeat_each_label_per_cv_split),' m: ', num2str(m), ...
     ' #Cells: ', num2str(length(ds.sites_to_use)), ' Augmentation factor: ', num2str(aug_factor), ' Tr/Te: ',num2str(length(all_YTr)),'/',num2str(length(all_YTe))]);
 
-
+hold off;
 end
 
